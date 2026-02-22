be184e625488afcbf327c3fbf16343600c515332
ธีรธัช ถวิลหวัง <teeratach.t@saifa.ai>
2026-02-23 02:01:44 +0700

    Update sidebar.tsx

--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -100,7 +100,7 @@ export function SidebarRoutes({
 
   return (
     <div className="flex flex-col w-full overflow-y-auto overflow-x-hidden">
-      <div className="px-3 py-2">
+      <div className="px-2 py-2">
         <h2
           className={cn(
             "mb-2 px-4 text-lg font-semibold tracking-tight whitespace-nowrap transition-all duration-300",
